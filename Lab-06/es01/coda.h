7e5ffcb7f12df6d48b8822aa11566b2334ec6258
Ciro <wellalollo@hotmail.it>
2014-11-18 22:34:05 +0100

    lib di coda.h

---
+++ Lab-06/es01/coda.h
@@ -0,0 +1,31 @@
+#define N 36
+
+typedef struct nodo3 *lista3;
+
+struct voti3
+{
+	int crediti3;
+	float media3;	
+	
+}voti3;
+
+struct nodo3
+{
+	lista3 prec3;
+	char nome3[N];
+	char cognome3[N];
+	char matricola3[N];
+	struct voti3 voto3;
+	lista3 succ3;
+	
+}nodo3;
+
+lista3 t3 ,first3  ,a3 ;
+
+
+void Nelementi3(int conta3);
+void Stampa3(lista3 t3, lista3 first3 , int conta3);
+lista3 Insert3(lista3 t3);
+void Destroy3(lista3 t3);
+lista3 EstrTesta2(lista3 t3 , lista3 first3 , int conta3);
+lista3 InsCoda3(lista3 t3 , lista3 first ,int conta3 , lista3 a3);
