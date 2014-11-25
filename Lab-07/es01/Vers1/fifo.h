b28d3f074fb91994b310843fea09e4d1a9be8217
Ciro <wellalollo@hotmail.it>
2014-11-25 22:32:26 +0100

    Create fifo.h

---
+++ Lab-07/es01/Vers1/fifo.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "item.h"
+
+void Card();
+void Full();
+void Empty();
+void Put(int n);
+void Get(int n);
+Item Init(int n);
+void Print(int n);
+void Save(int n);
+void Getf(int n);
